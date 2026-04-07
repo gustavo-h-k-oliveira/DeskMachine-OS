fc893f5e793c46094f413a7f7f3dd172652e5350
gustavo-h-k-oliveira <gustavo.oliveira204@fatec.sp.gov.br>
2026-04-07 15:39:24 -0300

    Adicionando ícones das aplicações; modularizando criação de atalhos e; atualizando módulo `Desktop` e arquivo CSS.

--- src/components/Desktop.jsx
+++ src/components/Desktop.jsx
@@ -1,7 +1,68 @@
+import DesktopShortcut from './DesktopShortcut';
+import Computer from '../assets/icons/computer.png';
+import Directory from '../assets/icons/directory.png';
+import WebPage from '../assets/icons/html.png';
+import Notepad from '../assets/icons/notepad.png';
+import Trash from '../assets/icons/recycle_bin.png';
+import Terminal from '../assets/icons/terminal.png';
+import Browser from '../assets/icons/internet.png';
+import Script from '../assets/icons/script.png';
+
 export default function Desktop() {
   return (
     <div className="app">
-      <h1>Bem-vindo ao <strong>DeskMachine OS</strong>!</h1>
+      <div className='desktop'>
+        <DesktopShortcut
+          icon={Computer}
+          label="Meu Computador"
+          onClick={() => alert('Abrindo Meu Computador...')}
+        />
+        <DesktopShortcut
+          icon={Directory}
+          label="Documentos"
+          onClick={() => alert('Abrindo Documentos...')}
+        />
+        <DesktopShortcut
+          icon={Notepad}
+          label="SobreMim.txt"
+          onClick={() => alert('Abrindo Sobre Mim...')}
+        />
+        <DesktopShortcut
+          icon={WebPage}
+          label="Contatos.html"
+          onClick={() => alert('Abrindo Contatos...')}
+        />
+        <DesktopShortcut
+          icon={Browser}
+          label="Internet Explorer"
+          onClick={() => alert('Abrindo Internet...')}
+        />
+        <DesktopShortcut
+          icon={Notepad}
+          label="Educação.txt"
+          onClick={() => alert('Abrindo Educação...')}
+        />
+        <DesktopShortcut
+          icon={WebPage}
+          label="Experiências.html"
+          onClick={() => alert('Abrindo Experiências...')}
+        />
+        <DesktopShortcut
+          icon={Script}
+          label="Habilidades"
+          onClick={() => alert('Abrindo Habilidades...')}
+        />
+        <DesktopShortcut
+          icon={Terminal}
+          label="Terminal"
+          onClick={() => alert('Abrindo Terminal...')}
+        />
+        <DesktopShortcut
+          icon={Trash}
+          label="Lixeira"
+          onClick={() => alert('Abrindo Lixeira...')}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/DesktopShortcut.jsx
@@ -0,0 +1,8 @@
+export default function DesktopShortcut({ icon, label, onClick }) {
+  return (
+    <button className="desktop-shortcut" onClick={onClick}>
+      <img src={icon} alt={label} className="desktop-shortcut__icon" />
+      <span className="desktop-shortcut__label">{label}</span>
+    </button>
+  );
+}
