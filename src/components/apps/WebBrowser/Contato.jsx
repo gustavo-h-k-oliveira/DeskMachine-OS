757cc701db59a30c861a8b5125111970c3aa3b4e
gustavo-h-k-oliveira <gustavo.oliveira204@fatec.sp.gov.br>
2026-04-16 14:52:47 -0300

    Adicionando aplicação `Contato.html` com arquivo CSS e; modularizando menu do navegador.

---
+++ src/components/apps/WebBrowser/Contato.jsx
@@ -0,0 +1,11 @@
+import "./WebBrowser.css";
+import Menu from "./Menu/Menu";
+
+export default function Contato() {
+  return (
+    <div className="frame">
+      <Menu />
+      <p>Web Browser</p>
+    </div>
+  );
+}
