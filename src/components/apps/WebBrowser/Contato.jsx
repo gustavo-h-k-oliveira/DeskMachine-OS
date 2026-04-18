7493019e426bf2efa3bbb291077861c3c64cba49
gustavo-h-k-oliveira <gustavo.oliveira204@fatec.sp.gov.br>
2026-04-17 22:53:56 -0300

    Adicionando conteúdo ao `Contato` e; implementando frame independente ao conteúdo de `Contato` e `Experiencia`.

--- src/components/apps/WebBrowser/Contato.jsx
+++ src/components/apps/WebBrowser/Contato.jsx
@@ -7,7 +7,12 @@ export default function Contato() {
     <div className="frame">
       <Menu link="http://gustavo.oliveira.com/contact/"/>
       <div className="contato-content">
-        Web Browser
+        <h1>Contatos</h1>
+        <p>WhatsApp: <a href="https://wa.me/5514981704947">(14) 98170-4947</a></p>
+        <p>LinkedIn: <a href="https://www.linkedin.com/in/gustavo-oliveira-713583214/">gustavo-oliveira</a></p>
+        <p>GitHub: <a href="https://github.com/gustavo-h-k-oliveira">gustavo-h-k-oliveira</a></p>
+        <p>E-mail: <a href="mailto:kenzojapan64@gmail.com">kenzojapan64@gmail.com</a></p>
+        <p>Instagram: <a href="https://www.instagram.com/gustavo.h.k.oliveira/">gustavo.h.k.oliveira</a></p>
       </div>
       <Footer />
     </div>
