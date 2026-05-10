1b722fedc24d0bc543c4fe1fb87e9a753d848658
gustavo-h-k-oliveira <gustavo.oliveira204@fatec.sp.gov.br>
2026-05-10 17:46:10 -0300

    Ajustando estilo do parágrafo em `Contato.css` e; corrigindo link do Instagram em `Contato.jsx`.

--- src/components/apps/WebBrowser/Contato.jsx
+++ src/components/apps/WebBrowser/Contato.jsx
@@ -9,11 +9,12 @@ export default function Contato() {
       <Menu link="http://gustavo.oliveira.com/contact/"/>
       <div className="contato-content">
         <h1>Contatos</h1>
+        <p>Bem-vindo ao meu cantinho da web! Navegue pelas minhas redes e deixe um recado.</p>
         <p><strong>WhatsApp</strong>: <a href="https://wa.me/5514981704947">(14) 98170-4947</a></p>
         <p><strong>LinkedIn</strong>: <a href="https://www.linkedin.com/in/gustavo-oliveira-713583214/">gustavo-oliveira</a></p>
         <p><strong>GitHub</strong>: <a href="https://github.com/gustavo-h-k-oliveira">gustavo-h-k-oliveira</a></p>
         <p><strong>E-mail</strong>: <a href="mailto:kenzojapan64@gmail.com">kenzojapan64@gmail.com</a></p>
-        <p><strong>Instagram</strong>: <a href="https://www.instagram.com/gustavo.h.k.oliveira/">gustavo.h.k.oliveira</a></p>
+        <p><strong>Instagram</strong>: <a href="https://www.instagram.com/gustavo.h.k.o/">gustavo.h.k.o</a></p>
       </div>
       <Footer />
     </div>
