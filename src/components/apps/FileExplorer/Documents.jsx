e5583b3304f6aab355cc2734f7d71130292c57b0
gustavo-h-k-oliveira <gustavo.oliveira204@fatec.sp.gov.br>
2026-05-12 15:34:39 -0300

    Adicionando componentes de File Explorer: Meu Computador, Documentos e Lixeira; ajustando estilo e funcionalidade no Desktop.

---
+++ src/components/apps/FileExplorer/Documents.jsx
@@ -0,0 +1,11 @@
+import './Documents.css';
+
+export default function Documents() {
+    return (
+        <div className="documents-frame">
+            <h1>Documentos</h1>
+            <p>Aqui você pode acessar seus documentos e arquivos importantes.</p>
+            <p>Em breve, esta funcionalidade estará disponível!</p>
+        </div>
+    );
+}
